0a19509abcaa32888c74780b2d32034037876a0e
felipenougues <felipenougues1@gmail.com>
2022-05-23 21:01:42 -0400

    primer commit

---
+++ public/js/app.js
@@ -0,0 +1,29 @@
+const path = require("path");
+
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+const archHome = path.join(__dirname, "/views.home.html")
+
+
+app.get("/", function(req, res) {
+    res.sendFile(archHome)
+})
+
+app.get("/login", function(req, res) {
+    res.sendFile(__dirname + '/views/login.html')
+})
+
+app.get("/register", function(req, res) {
+    res.sendFile(__dirname + '/views/register.html')
+})
+
+
+
+app.use(express.static("public"));
+
+app.listen(PORT, () => {
+    console.log("Servidor Corriendo")
+});
