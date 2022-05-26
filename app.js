bc3a52388ae1e8f41d369a057b83f8571d816773
felipenougues <felipenougues1@gmail.com>
2022-05-25 21:28:20 -0400

    otro public

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 const express = require("express");
 const app = express();
-app.use(express.static("./public"));
+app.use(express.static("/public"));
 const PORT = process.env.PORT || 3000;
 
 const archHome = path.join(__dirname, "/views/home.html")
