a526b42bb367d2a292531b10a3f03e960fc16c43
felipenougues <felipenougues1@gmail.com>
2022-05-25 21:26:46 -0400

    public

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 const express = require("express");
 const app = express();
-app.use(express.static("public"));
+app.use(express.static("./public"));
 const PORT = process.env.PORT || 3000;
 
 const archHome = path.join(__dirname, "/views/home.html")
@@ -19,9 +19,6 @@ app.get("/login", function(req, res) {
 app.get("/register", function(req, res) {
     res.sendFile(path.join(__dirname + '/views/register.html'))
 })
-
-
-
 
 
 app.listen(PORT, () => {
