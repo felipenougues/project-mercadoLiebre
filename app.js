3f488512e9dc5057e6bcff4cc71f091dfb6fe3ac
felipenougues <felipenougues1@gmail.com>
2022-05-29 21:03:12 -0400

    mm

--- app.js
+++ app.js
@@ -7,12 +7,21 @@ app.use('/public', express.static(path.join(__dirname, 'public')));
 const PORT = process.env.PORT || 3030;
 
 const archHome = path.join(__dirname, '/views/home.html')
+const archRegister = path.join(__dirname, '/views/register.html')
+const archLogin = path.join(__dirname, '/views/login.html')
 
 
 app.get('/', function(req, res) {
     res.sendFile(archHome)
 })
+app.get('/', function(req, res) {
+    res.sendFile(archRegister)
+})
+app.get('/', function(req, res) {
+    res.sendFile(archLogin)
+})
 
+/*
 app.get('/views/login', function(req, res) {
     res.sendFile(path.join(__dirname, "/views/login.html"))
 })
@@ -21,6 +30,7 @@ app.get('/views/register', function(req, res) {
     res.sendFile(path.join(__dirname, "/views/register.html"))
 })
 
+*/
 
 app.listen(PORT, () => {
     console.log("Servidor Corriendo")
