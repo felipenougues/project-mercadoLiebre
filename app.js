c0a3b003f8a591e246e164094002ee0165d00e6a
felipenougues <felipenougues1@gmail.com>
2022-05-29 20:38:32 -0400

    ejemplo de web

--- app.js
+++ app.js
@@ -1,25 +1,26 @@
-const path = require('path');
-
 const express = require('express');
 const app = express();
 
+const path = require('path');
+app.use('/static', express.static(path.join(__dirname, 'public')));
+
 const PORT = process.env.PORT || 3030;
 
-const archHome = path.join(__dirname, "/views/home.html")
+const archHome = path.join(__dirname, '/views/home.html')
 
 
-app.get("/", function(req, res) {
+app.get('/', function(req, res) {
     res.sendFile(archHome)
 })
 
-app.get("/views/login", function(req, res) {
+app.get("/login", function(req, res) {
     res.sendFile(path.join(__dirname, "/views/login.html"))
 })
 
-app.get("/views/register", function(req, res) {
-    res.sendFile(path.join(__dirname, "https://mercadoliebre-digitalh.herokuapp.com/views/register.html"))
+app.get("/register", function(req, res) {
+    res.sendFile(path.join(__dirname, "views/register.html"))
 })
-app.use(express.static("public"));
+
 
 app.listen(PORT, () => {
     console.log("Servidor Corriendo")
