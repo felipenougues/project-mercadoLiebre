df50740872d609262bbed5f8924894aded796495
felipenougues <felipenougues1@gmail.com>
2022-05-29 20:43:38 -0400

    h

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 const path = require('path');
-app.use('/static', express.static(path.join(__dirname, 'public')));
+app.use('/public', express.static(path.join(__dirname, 'public')));
 
 const PORT = process.env.PORT || 3030;
 
